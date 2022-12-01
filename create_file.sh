6ced2610320c28f4cd5a18c8d4ba01b988edbc9f
Isabelle Lafont <isalafont@gmail.com>
2022-12-01 11:19:32 +0100

    init

---
+++ create_file.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+DIR=$(dirname $(readlink -f $0))
+
+for y in `seq -w 2022 2022`; do
+  mkdir $DIR/$y
+
+  for i in `seq -w 1 25`; do
+    mkdir $DIR/$y/day-$i
+    touch $DIR/$y/day-$i/README.md
+    touch $DIR/$y/day-$i/day-$i-part-1.rb
+    if [ $i -ne '25' ]
+    then
+      touch $DIR/$y/day-$i/day-$i-part-2.rb
+    fi
+    touch $DIR/$y/day-$i/day-$i-input.txt
+    chmod +x $y/day-$i/*.rb
+  done
+done

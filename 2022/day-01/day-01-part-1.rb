5fa42a9f19cee1b845b5a0b5946dba28effb9d99
Isabelle Lafont <isalafont@gmail.com>
2022-12-01 12:07:25 +0100

    day 1

--- 2022/day-01/day-01-part-1.rb
+++ 2022/day-01/day-01-part-1.rb
@@ -1,8 +1,10 @@
 #!/usr/bin/env ruby
 
 file_path = File.expand_path("../day-01-input.txt", __FILE__)
-input     = File.read(file_path)
+input     = File.read(file_path).split("\n\n")
 
-calories = input.split("\n").map(&:to_i)
+quantities = input.each.map do |calorie|
+  calorie.each_line.map(&:to_i).sum
+  end
 
-puts calories
+puts quantities.max
